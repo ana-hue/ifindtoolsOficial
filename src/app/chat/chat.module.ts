276e271e7017fabcd4e8487add05a3d744a9a36f
kevinguizoni <58188726+kevinguizoni@users.noreply.github.com>
2020-11-04 14:56:45 -0300

    push
    chat

---
+++ src/app/chat/chat.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ChatPageRoutingModule } from './chat-routing.module';
+
+import { ChatPage } from './chat.page';
+
+import {AutosizeModule} from 'ngx-autosize';
+
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ChatPageRoutingModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ChatPage
+      }
+    ]),
+    AutosizeModule
+  ],
+  declarations: [ChatPage]
+})
+
+export class ChatPageModule {}
